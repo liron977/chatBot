db7a743fbf7d0c73bf658fc0a4d068690bbd5176
Liron aviv Kotev <lironkt@mta.ac.il>
2023-09-29 15:04:06 +0300

    add chat gpt

---
+++ src/main/java/com/example/demo/NetworkUtils.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+public class NetworkUtils {
+    public static void setUpProxy(String host, String port) {
+        System.setProperty("http.proxyHost", host);
+        System.setProperty("http.proxyPort", port);
+        System.setProperty("https.proxyHost", host);
+        System.setProperty("https.proxyPort", port);
+    }
+}
